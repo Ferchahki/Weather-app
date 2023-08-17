323c79162f7924d7f128703cc17ee84ab616fd29
Ferchahki <alawiabdo000@gmail.com>
2023-08-17 22:13:44 +0100

    Vue Project

---
+++ weather-app/vue-project/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#0abde3",
+        "weather-secondary": "#004E71",
+      },
+      fontFamily: {
+        Roboto: ["Roboto, sans-serif"],
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+      },
+    },
+  },
+  plugins: [],
+}
+
