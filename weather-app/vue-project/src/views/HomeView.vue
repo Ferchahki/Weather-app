868121659dd6db6fa4bded95c43739cf2772f2b0
Ferchahki <alawiabdo000@gmail.com>
2023-08-19 15:50:36 +0100

    fixe Home

--- weather-app/vue-project/src/views/HomeView.vue
+++ weather-app/vue-project/src/views/HomeView.vue
@@ -1,27 +1,54 @@
 <script setup>
 // import { RouterLink } from 'vue-router';
+import { ref } from "vue";
+import axios from "axios";
 
+const mapboxAPIKey =
+  "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      );
+      mapboxSearchResults.value = result.data.features;
+
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
 </script>
 
 <template>
-  <!-- <header class="sticky top-0 bg-weather-primary shadow-lg"> 
-    <nav class="container flex flex-col sm:flex-row items-center
-        gap-4 text-white py-8">
-      <RouterLink :to="{ name:'home' }">
-      <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-cloud-moon"></i>
-        <p class="text-2xl">The Local Weather</p>
-      </div>
-      </RouterLink>
-      <div class="flex gap-4 flex-1 justify-end">
-        <i class="fa-solid fa-info text-xl hover:text-weather-secondary duration-150
-          cursor-pointer"></i>
-          <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150
-          cursor-pointer
-          "></i>
-      </div>
-    </nav>
-   
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+          @click="previewCity(searchResult)"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
     
-  </header> -->
+  </main>
 </template>
