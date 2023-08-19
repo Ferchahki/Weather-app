22b1db0e86b0e2849662ab83e64b4dc19983a3ef
Ferchahki <alawiabdo000@gmail.com>
2023-08-19 23:29:23 +0100

    fix some error

---
+++ weather-app/vue-project/src/views/CityView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <Suspense>
+        <AsyncCityView/>
+        <template #fallback>
+            <p>Searching...</p>
+        </template>
+    </Suspense>
+    </div>
+</template>
+
+<script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
